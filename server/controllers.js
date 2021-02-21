80848e88adae53138cfec0baea13d4445ff6bb45
mefekaskaya <mefekaskaya@github.com>
2021-02-21 23:56:38 +0300

    controllers and routes created to get todo list and delete todo item created and tested in postman

--- server/controllers.js
+++ server/controllers.js
@@ -13,3 +13,22 @@ exports.create = async (req, res, next) => {
     console.log(error);
   }
 };
+
+exports.getAll = async (req, res, next) => {
+  try {
+    const todos = await Todo.find();
+    return res.json(todos);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+exports.delete = async (req, res, next) => {
+  try {
+    const todo = await Todo.findById(req.params.todoId);
+    await todo.remove();
+    return res.json({ todoId: req.params.todoId });
+  } catch (err) {
+    console.log(err);
+  }
+};
--- server/routes.js
+++ server/routes.js
@@ -2,5 +2,7 @@ const router = require("express").Router();
 const controllers = require("./controllers");
 
 router.post("/", controllers.create);
+router.get("/", controllers.getAll);
+router.post("/:todoId", controllers.delete);
 
 module.exports = router;
