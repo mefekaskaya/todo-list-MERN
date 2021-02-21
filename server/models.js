23ed3979a0261954895222b6cef5974b2887717f
mefekaskaya <mefekaskaya@github.com>
2021-02-21 23:47:48 +0300

    mongoose schema constituted fot todo item and mongo atlas uri added and secured

---
+++ server/models.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const todoSchema = new Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+  isCompleted: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  deadLine: {
+    type: Date,
+  },
+});
+
+module.exports = mongoose.model("Todo", todoSchema);
